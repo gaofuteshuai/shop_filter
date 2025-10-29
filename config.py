2cbd66b45e768a9b3f0b58a8e9c59b3cbab9e24d
gaofuteshuai <261690772@qq.com>
2025-10-30 05:46:30 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # MySQL数据库配置（所有文件共用此配置）
 MYSQL_CONFIG = {
     "host": "rm-bp1x8sx04c208v06sno.mysql.rds.aliyuncs.com",
-    "user": "dms_user_e06e46f",          
+    "user": "db_admin",          
     "password": "050306Yi$",  
     "database": "shop_db",     
     "port": 3306,
